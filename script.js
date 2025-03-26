e2561053c02ffabb7e1f3a2c8d2ad7ed84ce729a
kayumovmf <kayumowmf@gmail.com>
2025-03-26 10:48:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let num = prompt('String typedagi malumot kirirting');
+let number = prompt('Number typedagi malumot kirirting');
+let bool = prompt('Boolean typedagi malumotni kirirting');
+let info = prompt('Hamma malumotlarni togri kiritganingizga ishonchingiz komilmi?');
+let rrr = confirm(`siz kiritgan ${num} sozi srting typega tegishli                                                siz kiritgan ${1234} sozi number typega tegishli                                      siz kiritgan ${true} sozi boolean typega tegishli `)
